a75e05f82793e228fdcdf87079aa527ac79c2c74
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-09-18 20:26:22 -0400

    modified handlebars homepage

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,12 @@ const homeRoutes = require('./homeroutes');
 const api = require('./api');
 // const cloudinaryRoutes = require('./cloudinaryRoutes.js');
 
-
-router.use('/', homeRoutes);
 router.use('/api', api);
 // router.use('/test', cloudinaryRoutes);
+// router.use('/', cloudinaryRoutes);
+
+
+router.use('/', homeRoutes);
 
 module.exports = router;
 
